44d068a339e8d1698de4a13a4021a728cc390670
Nadux99 <nadullaknidu7@gmail.com>
2026-06-15 14:01:29 +0530

    fix: update server port to 5000

--- server.js
+++ server.js
@@ -128,6 +128,6 @@ app.delete("/users/:id", (req, res) => {
 
 
 // Start Server
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
